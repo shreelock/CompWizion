d7b4726d76cdff39eae246dc9f1bb3c4e2599570
shreelock <shreenath.dutt@gmail.com>
2017-10-04 17:15:45 -0400

    q1 and q2 done

--- hw2/main.py
+++ hw2/main.py
@@ -6,20 +6,25 @@ import sys
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
 
 def help_message():
-   print("Usage: [Question_Number] [Input_Options] [Output_Options]")
-   print("[Question Number]")
-   print("1 Perspective warping")
-   print("2 Cylindrical warping")
-   print("3 Bonus perspective warping")
-   print("4 Bonus cylindrical warping")
-   print("[Input_Options]")
-   print("Path to the input images")
-   print("[Output_Options]")
-   print("Output directory")
-   print("Example usages:")
-   print(sys.argv[0] + " 1 " + "[path to input image1] " + "[path to input image2] " + "[path to input image3] " + "[output directory]")
+    print("Usage: [Question_Number] [Input_Options] [Output_Options]")
+    print("[Question Number]")
+    print("1 Perspective warping")
+    print("2 Cylindrical warping")
+    print("3 Bonus perspective warping")
+    print("4 Bonus cylindrical warping")
+    print("[Input_Options]")
+    print("Path to the input images")
+    print("[Output_Options]")
+    print("Output directory")
+    print("Example usages:")
+    print(sys.argv[0] + " 1 " + "[path to input image1] " +
+          "[path to input image2] " + "[path to input image3] " +
+          "[output directory]")
+
 
 '''
 Detect, extract and match features between img1 and img2.
@@ -40,50 +45,55 @@ Usage example:
     plt.imshow(im2)
     plt.scatter(pts1[:,:,0],pts1[:,:,1], 0.5, c='r', marker='x')
 '''
+
+
 def feature_matching(img1, img2, savefig=False):
     # Initiate SIFT detector
     sift = cv2.xfeatures2d.SIFT_create()
     # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
     # FLANN parameters
     FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks=50)   # or pass empty dictionary
-    flann = cv2.FlannBasedMatcher(index_params,search_params)
-    matches2to1 = flann.knnMatch(des2,des1,k=2)
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)  # or pass empty dictionary
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches2to1 = flann.knnMatch(des2, des1, k=2)
 
-    matchesMask_ratio = [[0,0] for i in xrange(len(matches2to1))]
+    matchesMask_ratio = [[0, 0] for i in xrange(len(matches2to1))]
     match_dict = {}
-    for i,(m,n) in enumerate(matches2to1):
-        if m.distance < 0.7*n.distance:
-            matchesMask_ratio[i]=[1,0]
+    for i, (m, n) in enumerate(matches2to1):
+        if m.distance < 0.7 * n.distance:
+            matchesMask_ratio[i] = [1, 0]
             match_dict[m.trainIdx] = m.queryIdx
 
     good = []
-    recip_matches = flann.knnMatch(des1,des2,k=2)
-    matchesMask_ratio_recip = [[0,0] for i in xrange(len(recip_matches))]
+    recip_matches = flann.knnMatch(des1, des2, k=2)
+    matchesMask_ratio_recip = [[0, 0] for i in xrange(len(recip_matches))]
 
-    for i,(m,n) in enumerate(recip_matches):
-        if m.distance < 0.7*n.distance: # ratio
-            if m.queryIdx in match_dict and match_dict[m.queryIdx] == m.trainIdx: #reciprocal
+    for i, (m, n) in enumerate(recip_matches):
+        if m.distance < 0.7 * n.distance:  # ratio
+            if m.queryIdx in match_dict and match_dict[m.
+                                                       queryIdx] == m.trainIdx:  #reciprocal
                 good.append(m)
-                matchesMask_ratio_recip[i]=[1,0]
-
-
+                matchesMask_ratio_recip[i] = [1, 0]
 
     if savefig:
-        draw_params = dict(matchColor = (0,255,0),
-                           singlePointColor = (255,0,0),
-                           matchesMask = matchesMask_ratio_recip,
-                           flags = 0)
-        img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,recip_matches,None,**draw_params)
+        draw_params = dict(
+            matchColor=(0, 255, 0),
+            singlePointColor=(255, 0, 0),
+            matchesMask=matchesMask_ratio_recip,
+            flags=0)
+        img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, recip_matches, None,
+                                  **draw_params)
 
-        plt.figure(),plt.xticks([]),plt.yticks([])
-        plt.imshow(img3,)
-        plt.savefig("feature_matching.png",bbox_inches='tight')
+        plt.figure(), plt.xticks([]), plt.yticks([])
+        plt.imshow(img3, )
+        plt.savefig("feature_matching.png", bbox_inches='tight')
 
-    return ([ kp1[m.queryIdx].pt for m in good ],[ kp2[m.trainIdx].pt for m in good ])
+    return ([kp1[m.queryIdx].pt
+             for m in good], [kp2[m.trainIdx].pt for m in good])
+
 
 '''
 Warp an image from cartesian coordinates (x, y) into cylindrical coordinates (theta, h)
@@ -99,25 +109,27 @@ Usage example:
     K = np.array([[f, 0, w/2], [0, f, h/2], [0, 0, 1]]) # mock calibration matrix
     imcyl = cylindricalWarpImage(im, K)
 '''
-def cylindricalWarpImage(img1, K, savefig=False):
-    f = K[0,0]
 
-    im_h,im_w = img1.shape
+
+def cylindricalWarpImage(img1, K, savefig=False):
+    f = K[0, 0]
+
+    im_h, im_w = img1.shape
 
     # go inverse from cylindrical coord to the image
     # (this way there are no gaps)
     cyl = np.zeros_like(img1)
     cyl_mask = np.zeros_like(img1)
-    cyl_h,cyl_w = cyl.shape
+    cyl_h, cyl_w = cyl.shape
     x_c = float(cyl_w) / 2.0
     y_c = float(cyl_h) / 2.0
-    for x_cyl in np.arange(0,cyl_w):
-        for y_cyl in np.arange(0,cyl_h):
+    for x_cyl in np.arange(0, cyl_w):
+        for y_cyl in np.arange(0, cyl_h):
             theta = (x_cyl - x_c) / f
-            h     = (y_cyl - y_c) / f
+            h = (y_cyl - y_c) / f
 
             X = np.array([math.sin(theta), h, math.cos(theta)])
-            X = np.dot(K,X)
+            X = np.dot(K, X)
             x_im = X[0] / X[2]
             if x_im < 0 or x_im >= im_w:
                 continue
@@ -126,15 +138,15 @@ def cylindricalWarpImage(img1, K, savefig=False):
             if y_im < 0 or y_im >= im_h:
                 continue
 
-            cyl[int(y_cyl),int(x_cyl)] = img1[int(y_im),int(x_im)]
-            cyl_mask[int(y_cyl),int(x_cyl)] = 255
-
+            cyl[int(y_cyl), int(x_cyl)] = img1[int(y_im), int(x_im)]
+            cyl_mask[int(y_cyl), int(x_cyl)] = 255
 
     if savefig:
         plt.imshow(cyl, cmap='gray')
-        plt.savefig("cyl.png",bbox_inches='tight')
+        plt.savefig("cyl.png", bbox_inches='tight')
 
-    return (cyl,cyl_mask)
+    return (cyl, cyl_mask)
+
 
 '''
 Calculate the geometric transform (only affine or homography) between two images,
@@ -160,15 +172,18 @@ Usage example:
     plt.imshow(out, cmap='gray')
     plt.show()
 '''
-def getTransform(src, dst, method='affine'):
-    pts1,pts2 = feature_matching(src,dst)
 
-    src_pts = np.float32(pts1).reshape(-1,1,2)
-    dst_pts = np.float32(pts2).reshape(-1,1,2)
+
+def getTransform(src, dst, method='affine'):
+    pts1, pts2 = feature_matching(src, dst)
+
+    src_pts = np.float32(pts1).reshape(-1, 1, 2)
+    dst_pts = np.float32(pts2).reshape(-1, 1, 2)
 
     if method == 'affine':
-        M, mask = cv2.estimateAffine2D(src_pts, dst_pts, cv2.RANSAC, ransacReprojThreshold=5.0)
-        M = np.append(M, [[0,0,1]], axis=0)
+        M, mask = cv2.estimateAffine2D(
+            src_pts, dst_pts, cv2.RANSAC, ransacReprojThreshold=5.0)
+        # M = np.append(M, [[0, 0, 1]], axis=0)
 
     if method == 'homography':
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
@@ -176,63 +191,129 @@ def getTransform(src, dst, method='affine'):
     matchesMask = mask.ravel().tolist()
 
     return (M, pts1, pts2, mask)
-   
+
+
 # ===================================================
 # ================ Perspective Warping ==============
 # ===================================================
 def Perspective_warping(img1, img2, img3):
-	
-	# Write your codes here
-	output_image = img1 # This is dummy output, change it to your output
-	
-	# Write out the result
-	output_name = sys.argv[5] + "output_homography.png"
-	cv2.imwrite(output_name, output_image)
-	
-	return True
-	
+
+    # Write your codes here
+    output_image = img1  # This is dummy output, change it to your output
+    # ================================================
+    imc = img1  #cv2.imread("../hw2/input1.png", 0)
+    imc = cv2.copyMakeBorder(imc, 200, 200, 500, 500, cv2.BORDER_CONSTANT)
+    imr = img2  #cv2.imread("../hw2/input2.png", 0)
+    iml = img3  #cv2.imread("../hw2/input3.png", 0)
+
+    (M, pts1, pts2, mask) = getTransform(imr, imc, "homography")
+    outcr = cv2.warpPerspective(imr, M, (imc.shape[1], imc.shape[0]))
+
+    (M, pts1, pts2, mask) = getTransform(iml, imc, "homography")
+    outlc = cv2.warpPerspective(iml, M, (imc.shape[1], imc.shape[0]))
+
+    # cv2.namedWindow("imw", cv2.WINDOW_NORMAL)
+    # cv2.imshow("imw", outcr)
+    # cv2.waitKey()
+    # cv2.imshow("imw", imc)
+    # cv2.waitKey()
+    # cv2.imshow("imw", outlc)
+    # cv2.waitKey()
+
+    imfinal = np.maximum.reduce([outcr, imc, outlc])
+    # cv2.imshow("imw", imfinal)
+    # cv2.waitKey()
+
+    # imdemo = cv2.imread("../hw2/example_output1.png", 0)
+    # cv2.imshow("imw", imdemo)
+    # cv2.waitKey()
+
+    # ================================================
+    # Write out the resul
+    output_image = imfinal
+    output_name = sys.argv[5] + "output_homography.png"
+    cv2.imwrite(output_name, output_image)
+
+    return True
+
+
 def Bonus_perspective_warping(img1, img2, img3):
-	
-	# Write your codes here
-	output_image = img1 # This is dummy output, change it to your output
-	
-	# Write out the result
-	output_name = sys.argv[5] + "output_homography_lpb.png"
-	cv2.imwrite(output_name, output_image)
-	
-	return True
+
+    # Write your codes here
+    output_image = img1  # This is dummy output, change it to your output
+
+    # Write out the result
+    output_name = sys.argv[5] + "output_homography_lpb.png"
+    cv2.imwrite(output_name, output_image)
+
+    return True
+
 
 # ===================================================
 # =============== Cynlindrical Warping ==============
 # ===================================================
 def Cylindrical_warping(img1, img2, img3):
-	
-	# Write your codes here
-	output_image = img1 # This is dummy output, change it to your output
-	
-	# Write out the result
-	output_name = sys.argv[5] + "output_cylindrical.png"
-	cv2.imwrite(output_name, output_image)
-	
-	return True
+
+    # Write your codes here
+    output_image = img1  # This is dummy output, change it to your output
+    # ===============================================
+    f = 450
+    imcc = img1 #cv2.imread("../hw2/input1.png", 0)
+    h, w = imcc.shape
+    K = np.array([[f, 0, w / 2], [0, f, h / 2], [0, 0, 1]])
+    imcc, maskcc = cylindricalWarpImage(imcc, K)
+
+    imrc = img2 #cv2.imread("../hw2/input2.png", 0)
+    h, w = imrc.shape
+    K = np.array([[f, 0, w / 2], [0, f, h / 2], [0, 0, 1]])
+    imrc, maskrc = cylindricalWarpImage(imrc, K)
+
+    imlc = img3 #cv2.imread("../hw2/input3.png", 0)
+    h, w = imlc.shape
+    K = np.array([[f, 0, w / 2], [0, f, h / 2], [0, 0, 1]])
+    imlc, masklc = cylindricalWarpImage(imlc, K)
+
+    imcc = cv2.copyMakeBorder(imcc, 50, 50, 300, 300, cv2.BORDER_CONSTANT)
+    imrc = cv2.copyMakeBorder(imrc, 50, 50, 300, 300, cv2.BORDER_CONSTANT)
+    imlc = cv2.copyMakeBorder(imlc, 50, 50, 300, 300, cv2.BORDER_CONSTANT)
+
+    (M, pts1, pts2, mask) = getTransform(imrc, imcc)
+    outrc = cv2.warpAffine(imrc, M, (imcc.shape[1], imcc.shape[0]))
+
+    (M, pts1, pts2, mask) = getTransform(imlc, imcc)
+    outlc = cv2.warpAffine(imlc, M, (imcc.shape[1], imcc.shape[0]))
+
+
+    imfinal = np.maximum.reduce([outrc, imcc, outlc])
+    output_image = imfinal
+    # ===============================================
+    # Write out the result
+    output_name = sys.argv[5] + "output_cylindrical.png"
+    cv2.imwrite(output_name, output_image)
+
+    return True
+
 
 def Bonus_cylindrical_warping(img1, img2, img3):
-	
-	# Write your codes here
-	output_image = img1 # This is dummy output, change it to your output
-	
-	# Write out the result
-	output_name = sys.argv[5] + "output_cylindrical_lpb.png"
-	cv2.imwrite(output_name, output_image)
-	
-	return True
-	
+
+    # Write your codes here
+    output_image = img1  # This is dummy output, change it to your output
+
+    # Write out the result
+    output_name = sys.argv[5] + "output_cylindrical_lpb.png"
+    cv2.imwrite(output_name, output_image)
+
+    return True
+
+
 '''
 This exact function will be used to evaluate your results for HW2
 Compare your result with master image and get the difference, the grading
 criteria is posted on Piazza
 '''
-def RMSD(target, master):
+
+
+def RMSD(questionID, target, master):
     # Get width, height, and number of channels of the master image
     master_height, master_width = master.shape[:2]
     master_channel = len(master.shape)
@@ -242,44 +323,60 @@ def RMSD(target, master):
     target_channel = len(target.shape)
 
     # Validate the height, width and channels of the input image
-    if (master_height != target_height or master_width != target_width or master_channel != target_channel):
+    if (master_height != target_height or master_width != target_width
+            or master_channel != target_channel):
         return -1
     else:
-        total_diff = 0.0;
-        master_channels = cv2.split(master);
-        target_channels = cv2.split(target);
+        nonZero_target = cv2.countNonZero(target)
+        nonZero_master = cv2.countNonZero(master)
+
+        if (questionID == 1):
+            if (nonZero_target < 1200000):
+                return -1
+        elif (questionID == 2):
+            if (nonZero_target < 700000):
+                return -1
+        else:
+            return -1
+
+        total_diff = 0.0
+        master_channels = cv2.split(master)
+        target_channels = cv2.split(target)
 
         for i in range(0, len(master_channels), 1):
             dst = cv2.absdiff(master_channels[i], target_channels[i])
             dst = cv2.pow(dst, 2)
             mean = cv2.mean(dst)
-            total_diff = total_diff + mean[0]**(1/2.0)
+            total_diff = total_diff + mean[0]**(1 / 2.0)
 
-        return total_diff;
+        return total_diff
+
 
 if __name__ == '__main__':
-   question_number = -1
-   
-   # Validate the input arguments
-   if (len(sys.argv) != 6):
-      help_message()
-      sys.exit()
-   else: 
-      question_number = int(sys.argv[1])
-      if (question_number > 4 or question_number < 1):
-	 print("Input parameters out of bound ...")
-         sys.exit()
-		 
-   input_image1 = cv2.imread(sys.argv[2], 0)
-   input_image2 = cv2.imread(sys.argv[3], 0)
-   input_image3 = cv2.imread(sys.argv[4], 0) 
+    question_number = -1
 
-   function_launch = {
-   1 : Perspective_warping(input_image1, input_image2, input_image3),
-   2 : Cylindrical_warping(input_image1, input_image2, input_image3),
-   3 : Bonus_perspective_warping(input_image1, input_image2, input_image3),
-   4 : Bonus_cylindrical_warping(input_image1, input_image2, input_image3),
-   }
+    # Validate the input arguments
+    if (len(sys.argv) != 6):
+        help_message()
+        sys.exit()
+    else:
+        question_number = int(sys.argv[1])
+        if (question_number > 4 or question_number < 1):
+            print("Input parameters out of bound ...")
+            sys.exit()
 
-   # Call the function
-   function_launch[question_number]()
+    input_image1 = cv2.imread(sys.argv[2], 0)
+    input_image2 = cv2.imread(sys.argv[3], 0)
+    input_image3 = cv2.imread(sys.argv[4], 0)
+
+    function_launch = {
+        1: Perspective_warping,  #(input_image1, input_image2, input_image3),
+        2: Cylindrical_warping,  #(input_image1, input_image2, input_image3),
+        3:
+        Bonus_perspective_warping,  #(input_image1, input_image2, input_image3),
+        4:
+        Bonus_cylindrical_warping  #(input_image1, input_image2, input_image3),
+    }
+
+    # Call the function
+    function_launch[question_number](input_image1, input_image2, input_image3)
--- temp/2.py
+++ temp/2.py
@@ -144,7 +144,44 @@ def getTransform(src, dst, method='affine'):
     return (M, pts1, pts2, mask)
 
 #---------------------------------------------------------------
+def RMSD(questionID, target, master):
+    # Get width, height, and number of channels of the master image
+    master_height, master_width = master.shape[:2]
+    master_channel = len(master.shape)
 
+    # Get width, height, and number of channels of the target image
+    target_height, target_width = target.shape[:2]
+    target_channel = len(target.shape)
+
+    # Validate the height, width and channels of the input image
+    if (master_height != target_height or master_width != target_width
+            or master_channel != target_channel):
+        return -1
+    else:
+        nonZero_target = cv2.countNonZero(target)
+        nonZero_master = cv2.countNonZero(master)
+
+        if (questionID == 1):
+            if (nonZero_target < 1200000):
+                return -1
+        elif (questionID == 2):
+            if (nonZero_target < 700000):
+                return -1
+        else:
+            return -1
+
+        total_diff = 0.0
+        master_channels = cv2.split(master)
+        target_channels = cv2.split(target)
+
+        for i in range(0, len(master_channels), 1):
+            dst = cv2.absdiff(master_channels[i], target_channels[i])
+            dst = cv2.pow(dst, 2)
+            mean = cv2.mean(dst)
+            total_diff = total_diff + mean[0]**(1 / 2.0)
+
+        return total_diff
+#------------------------------------------------
 f = 450
 print f
 
@@ -197,3 +234,5 @@ cv2.waitKey()
 imdemo = cv2.imread("../hw2/example_output2.png",0)
 cv2.imshow("imw", imdemo)
 cv2.waitKey()
+
+print RMSD(2, imdemo, imfinal)
